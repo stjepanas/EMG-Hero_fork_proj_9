3951262dc7555249c56819aaa6c16625681f1c2d
stjepanas <165696485+stjepanas@users.noreply.github.com>
2024-12-22 00:05:32 +0100

    added a bit more to README and the plotting + testing in SL. Also changed the name of crrate_MDPDataseys to create_datasets cause the name was a bit misleading

--- SL.py
+++ SL.py
@@ -4,18 +4,21 @@ from libemg import streamers
 from libemg.feature_extractor import FeatureExtractor
 import numpy as np
 from libemg.data_handler import OfflineDataHandler, RegexFilter
-from libemg.data_handler import OnlineDataHandler
 from d3rlpy.algos import BCConfig
 import argparse
 from emg_hero.metrics import F1MacroEvaluator
 from d3rlpy import load_learnable
-from create_MDPDatasets import get_training_dataset, get_testing_dataset
+from create_datasets import get_training_dataset, get_testing_dataset
 from d3rlpy.dataset import MDPDataset
 from d3rlpy.constants import ActionSpace  
 from emg_hero.model_utility import build_algo 
 from emg_hero.configs import BaseConfig
 from gym_problem import reverse_mapping, mapping
-
+import os
+import matplotlib.pyplot as plt
+import csv
+import matplotlib.pyplot as plt
+from emg_hero.model_utility import get_newest_directory
 if __name__ == "__main__":
 
     # streamer, smm = streamers.sifi_bioarmband_streamer(
@@ -33,7 +36,7 @@ if __name__ == "__main__":
     # training_ui.start_gui()
 
     # folder where raw data from the armband is going to be stored
-    dataset_folder = 'data_stjepana'
+    dataset_folder = 'data'
     # gestures picked out for training, this is according to the gesture_list.json/collection.json
     gestures = ["0","1","2","3","4","5","6","7","8","9","10","11","12"]
     reps = ["0","1","2","3","4","5"]
@@ -49,8 +52,8 @@ if __name__ == "__main__":
     offline_dh.get_data(folder_location = dataset_folder, regex_filters=regex_filters, delimiter=",")
 
     # separates raw data into testing and training data
-    train_data = offline_dh.isolate_data("reps", [0,1,2,3]) # more data for training than for testing
-    test_data = offline_dh.isolate_data("reps", [4,5])
+    train_data = offline_dh.isolate_data("reps", [0]) # more data for training than for testing
+    test_data = offline_dh.isolate_data("reps", [4])
 
     # shapes data into windows x channels x nr of samples in a window, so nr_windows x 8 x 300
     # meta tells which movement is done for every window, gives a number from the gestures
@@ -72,33 +75,94 @@ if __name__ == "__main__":
     observations_test,actions_test,rewards_test,terminals_test,pretest_timeouts = get_testing_dataset(gestures , feature_list, size_windows_test, testing_features, test_meta)
 
     # returns the MDPDatasets
-    #dataset_pretrain = MDPDataset(observations_train,actions_train,rewards_train,terminals_train,pretrain_timeouts,action_space = ActionSpace.CONTINUOUS)
-    #dataset_pretest = MDPDataset(observations_test,actions_test,rewards_test,terminals_test,pretest_timeouts,action_space = ActionSpace.CONTINUOUS)
+    dataset_pretrain = MDPDataset(observations_train,actions_train,rewards_train,terminals_train,pretrain_timeouts,action_space = ActionSpace.CONTINUOUS)
+    dataset_pretest = MDPDataset(observations_test,actions_test,rewards_test,terminals_test,pretest_timeouts,action_space = ActionSpace.CONTINUOUS)
     
 ################ Behaviour cloning ################################################################
-#     supervised_train = dataset_pretrain
-#     supervised_test = dataset_pretest
-#     # takes the previous project's hyperparameters for the model
-#     base_config = BaseConfig()
+    #takes the previous project's hyperparameters for the model
+    base_config = BaseConfig()
+    model, floornoise = build_algo(pt_weights=None, pt_biases=None, base_config=base_config)
 
-#     model, floornoise = build_algo(pt_weights=None, pt_biases=None, base_config=base_config)
-
-#     f1_macro_evaluator = F1MacroEvaluator(supervised_test.episodes)
+    f1_macro_evaluator = F1MacroEvaluator(dataset_pretest.episodes)
     
-#     bc_model = model.fit(
-#       supervised_train,
-#       n_steps=1000,
-#       n_steps_per_epoch=50,
-#       evaluators={
-#           "f1_macro": f1_macro_evaluator,
-#      }
-#  )
-    # observation = observations_test
-    # gt_actions = actions_test
-    # # bc_model = load_learnable(r"d3rlpy_logs\BC_20241216175424\model_100000.d3")
-    # model_path = r"d3rlpy_logs\BC_20241216161532\model_1000.d3"
-    # bc_model = load_learnable(model_path)
-    # actions = bc_model.predict(observation)
+    bc_model = model.fit(
+      dataset_pretrain,
+      n_steps=1000,
+      n_steps_per_epoch=50,
+      evaluators={
+          "f1_macro": f1_macro_evaluator,
+     }
+ )
+
+################### Testing and plotting the model results #################################
+    observation = observations_test
+    gt_actions = actions_test
+    actions = np.zeros((np.shape(observation)[0],7))
+    model_map = get_newest_directory('d3rlpy_logs') # gets the latest generated map
+    filenames = os.listdir(model_map)
+    model_names = [f for f in filenames if f.startswith("model")]
+    plot_stuff = np.zeros((len(model_names), 4))
+
+    loss = np.zeros((len(model_names),1))
+    with open(str(model_map)+'\loss.csv', mode ='r')as file: # gets the loss file from the newest map
+        csvFile = csv.reader(file)
+        for i,lines in enumerate(csvFile):
+                loss[i] = lines[2]
+
+
+# Loops through all the newly generated models, each model predicts the movements on the test dataset
+# and the amount of correct, incorrect and invalid actions is recorded (loss as well) and plotted
+    for idx, model in enumerate(model_names):
+        bc_model = load_learnable(os.path.join(model_map, model))
+        actions = bc_model.predict(observation)
+        actions = (actions >= 0.5).astype(int) # int under 0.5 = 0, int over 0.5 = 1
+        correct = 0
+        incorrect = 0
+        total = 0
+        invalid = 0
+        for i in range(observation.shape[0]):
+            total += 1
+            if tuple(actions[i]) in reverse_mapping.keys():  # Check if valid action
+                if np.array_equal(actions[i], gt_actions[i]):
+                    correct += 1
+                else:
+                    incorrect += 1
+            else:
+                invalid += 1
+        mod_loss = loss[idx]
+        # Store results for this model as percentages
+        plot_stuff[idx] = [correct/total, incorrect/total, invalid/total, float(mod_loss)]
+
+    correct = plot_stuff[:, 0]
+    incorrect = plot_stuff[:, 1]
+    invalid = plot_stuff[:, 2]
+    loss = plot_stuff[:, 3]
+    # for model names
+    model_numbers = [i + 1 for i in range(np.shape(plot_stuff)[0])] 
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(model_numbers, correct, marker='o', label='Correct', color='green')
+    plt.plot(model_numbers, incorrect, marker='o', label='Incorrect', color='red')
+    plt.plot(model_numbers, invalid, marker='o', label='Invalid', color='orange')
+    plt.plot(model_numbers, loss, marker='o', label='Loss', color='blue')
+
+    plt.xlabel("Models")
+    plt.ylabel("Count")
+    plt.title("Evaluation Metrics for Models")
+    plt.legend()
+
+    plt.tight_layout()  
+    plt.show()
+
+
+   
+
+                     
+
+                 
+
+            
+
 
 
 
---
+++ create_datasets.py
@@ -0,0 +1,97 @@
+import numpy as np     
+from gym_problem import reverse_mapping, mapping
+
+def get_training_dataset(gestures: list, feature_list: list, size_windows_train, training_features, train_meta):
+    ''' Returns the necessary components to build the MDPDataset for training
+    Args: 
+        gestures(list): list of gestures that we want to pretrain the model on
+        feature_list(list): list of features that we want extracted
+        size_windows_train(int): number of windows in the training data
+        training_features: features from the feature list extracted from the raw data using libemgs 
+        extract_features function
+        train_meta: meta data for the training dataset from the parse_windows function    
+    '''
+
+    nr_windows = int(size_windows_train)
+    nr_channels = 8 # this is always the same so it can be hard-coded
+    
+    observations_train = np.zeros((nr_windows, len(feature_list)*nr_channels)) 
+    actions_train = np.zeros((nr_windows, 7))
+    rewards_train = np.zeros(nr_windows)
+    terminals_train = np.zeros((nr_windows))
+   
+   # checks for how long a movement is going on to set timeouts
+    counter = 0
+    for i in train_meta['classes']:
+        if i == int(gestures[0]):
+            counter += 1
+        else:
+            break
+   # sets timeouts at the beginning of a new movement
+    pretrain_timeouts = np.array([1 if ((i+1) % (counter) == 0 and i > 0) else 0 \
+                                     for i in range(actions_train.shape[0])]) 
+
+    # Build dataset
+    for i in range(nr_windows): 
+        feature_train_vector = []
+        for feature in feature_list:
+            # Concatenate features from all channels
+            feature_train_vector.extend(training_features[feature][i])
+  
+        observations_train[i] = feature_train_vector
+
+        gesture_train_class = train_meta['classes'][i] 
+        # maps the gesture from meta to the corresponding one hot pred vector
+        action = mapping[gesture_train_class] 
+        actions_train[i] = action['one_hot_pred']
+
+    return observations_train,actions_train,rewards_train,terminals_train,pretrain_timeouts
+    
+
+
+def get_testing_dataset(gestures:list , feature_list:list, size_windows_test, testing_features, test_meta):
+    ''' Returns the necessary components to build the MDPDataset for testing
+        Args: 
+            gestures(list): list of gestures that we want to pretrain the model on
+            feature_list(list): list of features that we want extracted
+            size_windows_test(int): number of windows in the testing data
+            testing_features: features from the feature list extracted from the raw data using libemgs 
+            extract_features function
+            test_meta: meta data for the testing dataset from the parse_windows function    
+    '''
+    nr_windows = int(size_windows_test)
+    nr_channels = 8
+    
+    observations_test = np.zeros((nr_windows, len(feature_list)*nr_channels)) 
+    actions_test = np.zeros((nr_windows, 7))
+
+    rewards_test = np.zeros(nr_windows)
+    terminals_test = np.zeros((nr_windows))
+   
+    # checks for how long a movement is going on to set timeouts
+    counter = 0
+    for i in test_meta['classes']:
+        if i == int(gestures[0]):
+            counter += 1
+        else:
+            break
+
+    pretest_timeouts = np.array([1 if ((i+1) % (counter) == 0 and i > 0) else 0 \
+                                     for i in range(actions_test.shape[0])]) 
+
+    # Build dataset
+    for i in range(nr_windows): 
+    # Extract features for the current window
+        feature_test_vector = []
+        for feature in feature_list:
+            # Concatenate features from all channels
+            feature_test_vector.extend(testing_features[feature][i])
+
+        observations_test[i] = feature_test_vector
+
+        gesture_test_class = test_meta['classes'][i] 
+        # maps the gesture from meta to the corresponding one hot pred vector
+        action = mapping[gesture_test_class]
+        actions_test[i] = action['one_hot_pred']
+
+    return observations_test,actions_test,rewards_test,terminals_test,pretest_timeouts
